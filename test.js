b91e4701f32799b2894e12629a6c80ab0ff27880
bobosheep <ianpaul32@gmail.com>
2017-11-01 18:39:35 +0800

    snake game

--- test.js
+++ test.js
@@ -1,10 +1,112 @@
 $(function(){
-	$("div").mouseenter(function(){
-		height += 50px;
-		width += 50px;
+	/*$("div").mouseenter(function(){
+		$(this).fadeTo('slow', 0.5);
 	});
-	$("div").mouseleave(function(){
-		height -= 50px;
-		width -= 50px;
+	$('div').mouseleave(function(){
+		$(this).toggle();
+	});*/
+	$('div').mouseenter(function(){
+		$(this).animate({
+			width: '150px', 
+			height: '150px'
+		}, 500);
 	});
-});	
+	$('div').mouseleave(function(){
+		$(this).animate({
+			width: '100px',
+			height: '100px'
+		}, 500);
+	});
+});
+px=py=10;
+gs=tc=20;
+ax=ay=15;
+xv=yv=0;
+trail=[];
+tail=5;
+$(document).ready(function(){
+	canv = $('.bg');
+	ctx = $('.bg')[0].getContext('2d');
+	$(document).on('keydown', function( event ){
+		switch(event.which){
+			case 37:
+				if(xv == 1 && yx == 0){
+					break;
+				}
+				xv=-1;yv=0;
+				break;
+			case 38:
+				if(xv == 0 && yx == 1){
+					break;
+				}
+				xv=0;yv=-1;
+				break;
+			case 39:
+				if(xv == -1 && yx == 0){
+					break;
+				}
+				xv=1;yv=0;
+				break;
+			case 40:
+				if(xv == 0 && yx == -1){
+					break;
+				}
+				xv=0;yv=1;
+				break;
+		}
+	});
+	setInterval(function(){
+		px += xv;
+		py += yv;
+		if(px < 0){
+			px = tc - 1;
+		}
+		if(px > tc - 1){
+			px = 0;
+		}
+		if(py < 0){
+			py = tc - 1;
+		}
+		if(py > tc - 1){
+			py = 0;
+		}
+		
+		ctx.fillStyle='black';
+		ctx.fillRect(0, 0, canv.width(), canv.height());
+		
+		ctx.fillStyle='lime';
+		for(var i = 0 ; i < trail.length; i++){
+			//if(i == trail.length - 1){
+				ctx.beginPath(); 
+				ctx.arc(trail[i].x* gs + gs / 2, trail[i].y * gs + gs / 2, gs / 2, 0, 2 * Math.PI);
+				ctx.fillStyle='lime';
+				ctx.closePath(); 
+				ctx.fill(); 
+				//continue;
+			//}
+			//ctx.fillRect(trail[i].x* gs, trail[i].y * gs, gs - 2, gs - 2);
+			if(trail[i].x == px && trail[i].y == py){
+				tail = 5;
+				score = 0;
+			}
+		}
+		
+		ctx.fillStyle='red';
+		ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
+		
+		trail.push({x:px, y:py});
+		while(trail.length > tail){
+			trail.shift();
+		}
+		
+		if(ax == px && ay == py){
+			tail++;
+			score += 20;
+			ax = Math.floor(Math.random() * tc);
+			ay = Math.floor(Math.random() * tc);
+		}
+		
+		$(p).html('Your score is ' + score);
+		
+	}, 1000/15);
+});
