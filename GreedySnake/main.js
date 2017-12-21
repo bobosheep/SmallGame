df2635c8abaa3f4c2d8668f211f7a8aef9336c04
bobosheep <ianpaul32@gmail.com>
2017-12-21 11:37:04 +0800

    new

--- GreedySnake/main.js
+++ GreedySnake/main.js
@@ -7,7 +7,97 @@ trail=[];
 tail=5;
 start=0;
 highscore = 0;
-$(document).ready(function(){
+score = 0;
+level = {
+	"speed": 15,
+	"wall":0,
+	"reverse":1
+};
+
+var game = function(){
+	px += xv;
+	py += yv;
+	if(px < 0){
+		px = tc - 1;
+	}
+	if(px > tc - 1){
+		px = 0;
+	}
+	if(py < 0){
+		py = tc - 1;
+	}
+	if(py > tc - 1){
+		py = 0;
+	}
+	
+	ctx.fillStyle='black';
+	ctx.fillRect(0, 0, canv.width(), canv.height());
+	
+	ctx.fillStyle='lime';
+	
+	for(var i = 0 ; i < trail.length; i++){
+		//if(i == trail.length - 1){
+			ctx.beginPath(); 
+			ctx.arc(trail[i].x* gs + gs / 2, trail[i].y * gs + gs / 2, gs / 2, 0, 2 * Math.PI);
+			if(i == 0){
+				ctx.fillStyle='darkgreen';
+			}
+			else if (i == 1) {
+				ctx.fillStyle='green';
+			} 
+			else ctx.fillStyle='lime';
+			ctx.closePath(); 
+			ctx.fill(); 
+			//continue;
+		//}
+		//ctx.fillRect(trail[i].x* gs, trail[i].y * gs, gs - 2, gs - 2);
+		if(trail[i].x == px && trail[i].y == py){
+			tail = 5;
+			score = 0;
+			if(start)
+			{
+				alert('You Die!');
+			}
+		}
+	}
+	trail.push({x:px, y:py});
+	while(trail.length > tail){
+		trail.shift();
+	}
+	ctx.fillStyle='red';
+	ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
+	
+	
+	
+	if(ax == px && ay == py){
+		tail++;
+		score += 20;
+		var isOnsnake = true;
+		while(isOnsnake){
+			ax = Math.floor(Math.random() * tc);
+			ay = Math.floor(Math.random() * tc);
+			isOnsnake = false;
+			for(var i = 0 ; i < trail.length ; i++){
+				if(ax == trail[i].x && ay == trail[i].y){
+					isOnsnake = true;
+					break;
+				}
+				}
+		}
+	}
+	
+	$('#scoreboard').text('Score: ' + score);
+	if(score > highscore){
+		$('#highscore').text('High Score: ' + score);
+		highscore = score;
+	}
+	$('#snake-pos').text('Snake at ( ' + px + ' , ' + py + ' )');
+	$('#score-pos').text('Point at ( ' + ax + ' , ' + ay + ' )');
+	
+}
+
+var gamestart = function(){
+
 	canv = $('.bg');
 	ctx = $('.bg')[0].getContext('2d');
 	$('#left').on('click', function (){
@@ -38,7 +128,7 @@ $(document).ready(function(){
 		}
 		start = 1;
 	});
-	$(document).on('keydown', function( event ){
+	$(document).on('keydown',function( event ){
 		switch(event.which){
 			case 37:
 				if(xv == 1 && yv == 0){
@@ -74,85 +164,14 @@ $(document).ready(function(){
 				break;
 		}
 	});
-	setInterval(function(){
-		px += xv;
-		py += yv;
-		if(px < 0){
-			px = tc - 1;
-		}
-		if(px > tc - 1){
-			px = 0;
-		}
-		if(py < 0){
-			py = tc - 1;
-		}
-		if(py > tc - 1){
-			py = 0;
-		}
-		
-		ctx.fillStyle='black';
-		ctx.fillRect(0, 0, canv.width(), canv.height());
-		
-		ctx.fillStyle='lime';
-		
-		for(var i = 0 ; i < trail.length; i++){
-			//if(i == trail.length - 1){
-				ctx.beginPath(); 
-				ctx.arc(trail[i].x* gs + gs / 2, trail[i].y * gs + gs / 2, gs / 2, 0, 2 * Math.PI);
-				if(i == 0){
-					ctx.fillStyle='darkgreen';
-				}
-				else if (i == 1) {
-					ctx.fillStyle='green';
-				} 
-				else ctx.fillStyle='lime';
-				ctx.closePath(); 
-				ctx.fill(); 
-				//continue;
-			//}
-			//ctx.fillRect(trail[i].x* gs, trail[i].y * gs, gs - 2, gs - 2);
-			if(trail[i].x == px && trail[i].y == py){
-				tail = 5;
-				score = 0;
-				if(start)
-				{
-					alert('You Die!');
-				}
-			}
-		}
-		trail.push({x:px, y:py});
-		while(trail.length > tail){
-			trail.shift();
-		}
-		ctx.fillStyle='red';
-		ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
-		
-		
-		
-		if(ax == px && ay == py){
-			tail++;
-			score += 20;
-			var isOnsnake = true;
-			while(isOnsnake){
-				ax = Math.floor(Math.random() * tc);
-				ay = Math.floor(Math.random() * tc);
-				isOnsnake = false;
-				for(var i = 0 ; i < trail.length ; i++){
-					if(ax == trail[i].x && ay == trail[i].y){
-						isOnsnake = true;
-						break;
-					}
- 				}
-			}
-		}
-		
-		$('#scoreboard').text('Score: ' + score);
-		if(score > highscore){
-			$('#highscore').text('High Score: ' + score);
-			highscore = score;
-		}
-		$('#snake-pos').text('Snake at ( ' + px + ' , ' + py + ' )');
-		$('#score-pos').text('Point at ( ' + ax + ' , ' + ay + ' )');
-		
-	}, 1000/15);
-});
+	setInterval(game, 1000/15);
+}
+
+init = function(){
+	
+};
+
+$(document).ready(gamestart);
+
+
+
