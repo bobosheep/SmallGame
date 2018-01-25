07656c340be7a8f27461cf1004d7748eaee73e9e
bobosheep <ianpaul32@gmail.com>
2018-01-25 18:09:44 +0800

    update

--- CardGame/js/mainController.js
+++ CardGame/js/mainController.js
@@ -2,97 +2,154 @@ app.controller('mainController', ['$scope', function($scope){
     $scope.levels = [{
         level: '1',
         detail: '4 * 4',
+        col: '4',
+        row: '4'
     },
     {
         level: '2',
         detail: '5 * 6',
+        col: '5',
+        row: '6'
     },
     {
         level: '3',
         detail: '6 * 7',
+        col: '6',
+        row: '7'
     }];
-    $scope.go = function(levels){
-        window.location.href = 'level' + levels.level + '.html';
-    };
-    $scope.cards = [{
+    $scope.cards = [[{
         img: '1',
         name: 'orange',
         src: 'https://goo.gl/u7sXNm',
+        open: false,
+        pair: false
     },{
         img: '2',
         name: 'banana',
         src:  'https://goo.gl/YWmbS8',
+        open: false,
+        pair: false
+
     },{
         img: '3',
         name: 'apple',
         src:  'https://goo.gl/4HcdNb',
+        open: false,
+        pair: false
     },{
         img: '4',
         name: 'strawberry',
         src:  'https://goo.gl/VLGSVE',
-    },{
+        open: false,
+        pair: false
+    }],[{
         img: '5',
-        name: 'watermelon',
-        src:  'https://goo.gl/KDH9Yg',
+        name: 'orange',
+        src: 'https://goo.gl/u7sXNm',
+        open: false,
+        pair: false
     },{
         img: '6',
-        name: 'grape',
-        src:  'https://goo.gl/3of6tv',
+        name: 'banana',
+        src:  'https://goo.gl/YWmbS8',
+        open: false,
+        pair: false
     },{
         img: '7',
-        name: 'guava',
-        src:  'https://goo.gl/SdXajM',
+        name: 'apple',
+        src:  'https://goo.gl/4HcdNb',
+        open: false,
+        pair: false
     },{
         img: '8',
-        name: 'lemon',
-        src:  'https://goo.gl/pWgCvu',
-    },{
+        name: 'strawberry',
+        src:  'https://goo.gl/VLGSVE',
+        open: false,
+        pair: false
+    }],[{
         img: '9',
-        name: '',
-        src:  '',
+        name: 'watermelon',
+        src:  'https://goo.gl/KDH9Yg',
+        open: false,
+        pair: false
     },{
         img: '10',
-        name: '',
-        src:  '',
+        name: 'grape',
+        src:  'https://goo.gl/3of6tv',
+        open: false,
+        pair: false
     },{
         img: '11',
-        name: '',
-        src:  '',
+        name: 'guava',
+        src:  'https://goo.gl/SdXajM',
+        open: false,
+        pair: false
     },{
         img: '12',
-        name: '',
-        src:  '',
-    },{
+        name: 'lemon',
+        src:  'https://goo.gl/pWgCvu',
+        open: false,
+        pair: false
+    }],[{
         img: '13',
-        name: '',
-        src:  '',
+        name: 'watermelon',
+        src:  'https://goo.gl/KDH9Yg',
+        open: false,
+        pair: false
     },{
         img: '14',
-        name: '',
-        src:  '',
+        name: 'grape',
+        src:  'https://goo.gl/3of6tv',
+        open: false,
+        pair: false
     },{
         img: '15',
-        name: '',
-        src:  '',
+        name: 'guava',
+        src:  'https://goo.gl/SdXajM',
+        open: false,
+        pair: false
     },{
         img: '16',
-        name: '',
-        src:  '',
-    },{
-        img: '17',
-        name: '',
-        src:  '',
-    },{
-        img: '18',
-        name: '',
-        src:  '',
-    },{
-        img: '19',
-        name: '',
-        src:  '',
-    },{
-        img: '20',
-        name: '',
-        src:  '',
-    }];
+        name: 'lemon',
+        src:  'https://goo.gl/pWgCvu',
+        open: false,
+        pair: false
+    }]];
+    $scope.openCard = 0;
+    $scope.go = function(l){
+        window.location.href = 'level' + l.level + '.html';
+        
+    };
+    $scope.check = function(id){
+        $scope.openCard++;
+        id.open = true;
+        var img = '#img' + id.img;
+        $(img).attr("src", id.src);
+        if($scope.openCard === 3){
+            function sleep(milliseconds) {
+                var start = new Date().getTime();
+                for (var i = 0; i < 1e7; i++) {
+                    if ((new Date().getTime() - start) > milliseconds){
+                        break;
+                    }
+                }
+            }
+            sleep(1000);
+            $scope.openCard = 0;
+            for(let i = 0 ; i < 4 ; i++){
+                for(let j = 0; j < 4 ; j++){
+                    var tis = $scope.cards[i][j];
+                    if(tis.open === true && tis.pair === false){
+                        var ig = '#img' + tis.img;
+                        $(ig).attr("src", "https://goo.gl/fxQFZG");
+                        $scope.cards[i][j].open = false;
+                    }
+                }
+            }
+
+        
+        }
+        
+    };
+    
 }]);
