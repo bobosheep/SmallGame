c1ed18c7ef0d8e4169c4d4896b955a35529ddbdb
bobosheep <ianpaul32@gmail.com>
2018-01-26 17:20:41 +0800

    update
    fix layout and some bug

--- CardGame/js/mainController.js
+++ CardGame/js/mainController.js
@@ -17,170 +17,179 @@ app.controller('mainController', ['$scope', function($scope){
         col: '6',
         row: '7'
     }];
-    $scope.cards = [[{
+    $scope.cards = [{
         img: '1',
         name: 'orange',
         src: 'https://goo.gl/u7sXNm',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '2',
         name: 'banana',
         src:  'https://goo.gl/YWmbS8',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
-
+        pair: false,
+        state: true
     },{
         img: '3',
         name: 'apple',
         src:  'https://goo.gl/4HcdNb',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '4',
         name: 'strawberry',
         src:  'https://goo.gl/VLGSVE',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
-    }],[{
+        pair: false,
+        state: true
+    },{
         img: '5',
         name: 'orange',
         src: 'https://goo.gl/u7sXNm',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '6',
         name: 'banana',
         src:  'https://goo.gl/YWmbS8',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '7',
         name: 'apple',
         src:  'https://goo.gl/4HcdNb',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '8',
         name: 'strawberry',
         src:  'https://goo.gl/VLGSVE',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
-    }],[{
+        pair: false,
+        state: true
+    },{
         img: '9',
         name: 'watermelon',
         src:  'https://goo.gl/KDH9Yg',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '10',
         name: 'grape',
         src:  'https://goo.gl/3of6tv',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '11',
         name: 'guava',
         src:  'https://goo.gl/SdXajM',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '12',
         name: 'lemon',
         src:  'https://goo.gl/pWgCvu',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
-    }],[{
+        pair: false,
+        state: true
+    },{
         img: '13',
         name: 'watermelon',
         src:  'https://goo.gl/KDH9Yg',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '14',
         name: 'grape',
         src:  'https://goo.gl/3of6tv',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '15',
         name: 'guava',
         src:  'https://goo.gl/SdXajM',
         or_src: 'https://goo.gl/fxQFZG',
-        open: false,
-        pair: false
+        pair: false,
+        state: true
     },{
         img: '16',
         name: 'lemon',
         src:  'https://goo.gl/pWgCvu',//正面
         or_src: 'https://goo.gl/fxQFZG',//背面
-        open: false,
-        pair: false
-    }]];
+        pair: false,
+        state: true
+    }];
     $scope.openCard = 0;
+    $scope.finish = false;
     $scope.firstCard = '';
     $scope.go = function(l){
         window.location.href = 'level' + l.level + '.html';
         
     };
-    $scope.check = function(id){
-        if(id.open === false)
-        {
-            $scope.openCard++;
-            id.open = true;
+    $scope.shuffleCards = function(){
+        $scope.cards.sort(function(){ return 0.5 - Math.random()});
+    };
+    $scope.coverAllCards = function(){
+        for(let i = 0 ; i < 16 ; i++){
+           // for(let j = 0; j < 4 ; j++){
+                var tis = $scope.cards[i];
+                setTimeout(function(){
+                    $scope.cards[i].state = true;
+                }, 0);
+           // }
         }
-        id.or_src = id.src;
+        $scope.shuffleCards();
+    };
+    $scope.check = function(id){
+        if(id.state)   {
+            $scope.openCard++;
+            id.state = false;
+        }
         if($scope.openCard === 1){
             $scope.firstCard = id.name;
+            console.log(id.img);
         }
         else if($scope.openCard === 2){
             
             //sleep(1000);
             
             if(id.name === $scope.firstCard){
-                for(let i = 0 ; i < 4 ; i++){
-                    for(let j = 0; j < 4 ; j++){
-                        var tis = $scope.cards[i][j];
+                for(let i = 0 ; i < 16 ; i++){
+                    //for(let j = 0; j < 4 ; j++){
+                        var tis = $scope.cards[i];
                         if(tis.name === $scope.firstCard){
                             console.log(tis.name);
-                            $scope.cards[i][j].pair = true;
+                            $scope.cards[i].pair = true;
                             //alert('paired');
                         }
-                    }
+                   // }
                 }
             }
 
             $scope.openCard = 0;
             
-            //var tis = angular.forEach($scope.cards);
-            //console.log()
-         
             
-            for(let i = 0 ; i < 4 ; i++){
-                for(let j = 0; j < 4 ; j++){
-                    var tis = $scope.cards[i][j];
-                    if(tis.open === true && tis.pair === false){
+            for(let i = 0 ; i < 16 ; i++){
+                //for(let j = 0; j < 4 ; j++){
+                    var tis = $scope.cards[i];
+                    if(!tis.state && !tis.pair){
                         setTimeout(function(){
-                            $scope.cards[i][j].or_src = 'https://goo.gl/fxQFZG';
-                            $scope.cards[i][j].open = false;
-                        }, 1000);
+                            $scope.cards[i].state = true;
+                        }, 0);
                         
                     }
-                }
+                //}
             }
             
         
