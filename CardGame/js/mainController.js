19b387b8fd0439ae5e22d8d37d154ae2a86cbdf2
Ian Tsai <ianpaul32@gmail.com>
2018-02-09 00:00:20 +0800

    fix layout bugs

--- CardGame/js/mainController.js
+++ CardGame/js/mainController.js
@@ -21,119 +21,119 @@ app.controller('mainController', ['$scope', function($scope){
         img: '1',
         name: 'orange',
         src: 'https://goo.gl/u7sXNm',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '2',
         name: 'banana',
         src:  'https://goo.gl/YWmbS8',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '3',
         name: 'apple',
         src:  'https://goo.gl/4HcdNb',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '4',
         name: 'strawberry',
         src:  'https://goo.gl/VLGSVE',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '5',
         name: 'orange',
         src: 'https://goo.gl/u7sXNm',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '6',
         name: 'banana',
         src:  'https://goo.gl/YWmbS8',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '7',
         name: 'apple',
         src:  'https://goo.gl/4HcdNb',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '8',
         name: 'strawberry',
         src:  'https://goo.gl/VLGSVE',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '9',
         name: 'watermelon',
         src:  'https://goo.gl/KDH9Yg',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '10',
         name: 'grape',
         src:  'https://goo.gl/3of6tv',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '11',
         name: 'guava',
         src:  'https://goo.gl/SdXajM',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '12',
         name: 'lemon',
         src:  'https://goo.gl/pWgCvu',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '13',
         name: 'watermelon',
         src:  'https://goo.gl/KDH9Yg',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '14',
         name: 'grape',
         src:  'https://goo.gl/3of6tv',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '15',
         name: 'guava',
         src:  'https://goo.gl/SdXajM',
-        or_src: 'https://goo.gl/fxQFZG',
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',
         pair: false,
         state: true
     },{
         img: '16',
         name: 'lemon',
         src:  'https://goo.gl/pWgCvu',//正面
-        or_src: 'https://goo.gl/fxQFZG',//背面
+        or_src: 'https://upload.wikimedia.org/wikipedia/zh/thumb/5/5d/National_Chung_Cheng_University_logo.svg/1074px-National_Chung_Cheng_University_logo.svg.png',//背面
         pair: false,
         state: true
     }];
     $scope.openCard = -1;
     $scope.finish = false;
     $scope.firstCard = '';
-    //$scope.secondCard = '';
+    $scope.secondCard = '';
     $scope.start = true;
     $scope.timing = 0.0;
     var st;
@@ -156,6 +156,7 @@ app.controller('mainController', ['$scope', function($scope){
                 });
             }, 0);
         }
+
         $scope.timing = 0.0;
         $scope.start = true;
         $scope.openCard = -1;
@@ -167,67 +168,70 @@ app.controller('mainController', ['$scope', function($scope){
         window.location.href = 'level' + l.level + '.html';
         
     };
+    $scope.back = function(){
+        window.location.href = 'index.html';
+    };
     $scope.check = function(id){
         if($scope.openCard === -1){
             $scope.startClock();
-            $scope.openCard++;
+            $scope.openCard = 1;
         }
-        if(id.state)   {
-            $scope.openCard++;
-            id.state = false;
-            $scope.$apply();
-        }
-        if($scope.openCard === 1){
-            $scope.firstCard = id.name;
-            //console.log(id.img);
-        }
-        
-        else if($scope.openCard === 2){
-            $scope.secondCard = id.name;
-            console.log(id.img);
-            if($scope.secondCard === $scope.firstCard){
-                for(let i = 0 ; i < 16 ; i++){
-                   var tis = $scope.cards[i];
-                    if(tis.name === $scope.firstCard){
-                        console.log(tis.name);
-                        $scope.cards[i].pair = true;
-                        //alert('paired');
+
+        if(id.state){
+            if($scope.openCard === 2){
+                $scope.secondCard = id.name;
+                console.log(id.img);
+                if($scope.secondCard === $scope.firstCard){
+                    for(let i = 0 ; i < 16 ; i++){
+                    var tis = $scope.cards[i];
+                        if(tis.name === $scope.firstCard){
+                            console.log(tis.name);
+                            $scope.cards[i].pair = true;
+                        }
                     }
                 }
-            }
-            var cnt = 0;
-            for(let i = 0; i < 16 ; i++){
-                if($scope.cards[i].pair){
-                    cnt++;
+                var cnt = 0;
+                for(let i = 0; i < 16 ; i++){
+                    if($scope.cards[i].pair){
+                        cnt++;
+                    }
                 }
-            }
+                
+                if(cnt === 16){
+                    //$scope.$apply();
+                    window.alert('Finish'+ $scope.timing.toFixed(2));
+                    clearTimeout(st);
+                    cnt = 0;
+                }
+
             
-            if(cnt === 16){
-                $scope.$apply();
-                window.alert('Finish'+ $scope.timing.toFixed(2));
-                clearTimeout(st);
-                cnt = 0;
+                $scope.openCard--;
+                setTimeout(function(){
+                    $scope.$apply(function(){
+                       id.state = false;
+                    });
+                }, 0);
+
             }
-        }
-        
-        else if($scope.openCard === 3){
-            
-            $scope.openCard = 0;
-                        
-            for(let i = 0 ; i < 16 ; i++){
-                //for(let j = 0; j < 4 ; j++){
+            else if($scope.openCard === 1){
+                
+                for(let i = 0 ; i < 16 ; i++){
                     var tis = $scope.cards[i];
                     if(!tis.state && !tis.pair){
-                        //setTimeout(function(){
-                            $scope.cards[i].state = true;
-                        //}, 0);
+                        setTimeout(function(){
+                            $scope.$apply($scope.cards[i].state = true);
+                        }, 0);
                         
                     }
-                //}
+                }
+
+                $scope.firstCard = id.name;
+                $scope.openCard++;
+                setTimeout(function(){
+                    $scope.$apply(id.state = false);
+                }, 0);
             }
-            
         }
-        
         
     };
     
