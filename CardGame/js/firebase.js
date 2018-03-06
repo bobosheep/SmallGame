292760b789806177af6cf663d7ad8db68d004232
Ian Tsai <ianpaul32@gmail.com>
2018-03-06 13:45:06 +0800

    add PWA  smaller picture
    tiny picture

---
+++ CardGame/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCOmgbr2RCraK1DbFFgb2ft-usKmgo6HHc",
+    authDomain: "cardgame-87719.firebaseapp.com",
+    databaseURL: "https://cardgame-87719.firebaseio.com",
+    projectId: "cardgame-87719",
+    storageBucket: "cardgame-87719.appspot.com",
+    messagingSenderId: "204401887681"
+};
+firebase.initializeApp(config);
