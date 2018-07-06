c065522be9544c3192d256b3384c4d9413252058
Ian Tsai <ianpaul32@gmail.com>
2018-07-07 01:27:30 +0800

    update
    add score
    fix some bugs
    change init status

--- HitTheBrick/main.js
+++ HitTheBrick/main.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
+const status = {start: -1, playing: 0, gameover:1}
+
 
 let centerX = canvas.width / 2, centerY = canvas.height / 2;   
 let ballx = centerX, bally = centerY;
 let  paddleWidth = 80, paddleHeight = 10,    
     paddlex = centerX - paddleWidth / 2, paddley = canvas.height - 20; 
-let dx = 5, dy = 5;
+let dx = 2, dy = 2, paddledx = 3;
 let radius = 10;
 let lpress = false, rpress = false;
 let bricksOffsetLeft = 30, bricksOffsetTop = 30, bircksPadding = 10;
@@ -13,13 +15,21 @@ let bricksWidth = 50, bricksHeight = 20;
 let totalBricks = 15, bricksRow = 3, bricksColumn = 7;
 let bricks = [];
 
-(BricksInit = () => {
+let score = 0;
+let game_status = -1;
+
+(Init = () => {
     for(let i = 0 ; i < bricksRow ; i++){
         bricks[i] = []
         for(let j = 0 ; j < bricksColumn ; j++){
             bricks[i][j] = {x:0, y:0, status:1};
         }
     }
+    paddlex = centerX - paddleWidth / 2;
+    paddley = canvas.height - 20;
+    ballx = centerX;
+    bally = paddley - radius; 
+    game_status = -1;
 })();
 
 kdown = (e) => {
@@ -74,6 +84,12 @@ DrwBricks = () => {
     }
 }
 
+DrawScore = () => {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#050505";
+    ctx.fillText("Score : " + score, centerX - 30, 16 );
+}
+
 CollisionDetect = () =>{
     for(let i = 0 ; i < bricksRow ; i++){
         for(let j = 0 ; j < bricksColumn ; j++){
@@ -83,12 +99,13 @@ CollisionDetect = () =>{
                    ballx - radius < x + bricksWidth && ballx > x && bally < y + bricksHeight && bally >= y  ){
                    dx = -dx;
                    bricks[i][j].status = 0;
+                   score += 1;
                 }
                 else if(bally + radius > y && bally < y && ballx >= x && ballx <= x + bricksWidth   ||
                         bally - radius < y + bricksHeight && bally > y && ballx >= x && ballx <= x + bricksWidth){
                     dy = -dy;
                     bricks[i][j].status = 0;
-
+                    score += 1;
                 }
 
             }
@@ -103,21 +120,31 @@ Draw = () => {
     DrawPaddle();
     DrwBricks();
     CollisionDetect();
+    DrawScore();
+    if(game_status == status.start && (lpress || rpress)){
+        dx = rpress ? Math.abs(dx) : -Math.abs(dx);
+        game_status = 0;
+    }        
     if(lpress && paddlex > 0)  
-        paddlex -= 7;
+        paddlex -= paddledx;
     if(rpress && paddlex + paddleWidth < canvas.width)
-        paddlex += 7;
-    if(dx + ballx + radius > canvas.width || dx + ballx - radius < 0)
+        paddlex += paddledx;
+    if( ( ballx - radius < paddlex + paddleWidth && ballx > paddlex && bally >= paddley && bally <= paddley +  paddleHeight) ||
+        ( ballx + radius > paddlex && ballx < paddlex && bally >= paddley && bally <= paddley +  paddleHeight) ||
+        dx + ballx + radius > canvas.width || dx + ballx - radius < 0)
         dx = -dx;
-    if((bally + radius > paddley && ballx >= paddlex && ballx <= paddlex + paddleWidth && bally <= paddley)
+    if((bally + radius > paddley && ballx >= paddlex && ballx <= paddlex + paddleWidth && bally < paddley)
         || dy + bally - radius < 0)
         dy = -dy;
     if(dy + bally + radius > canvas.height){
         ballx = centerX, bally = centerY;
-        BricksInit();
+        Init();
+        score = 0;
     }
-        
-    ballx += dx;
-    bally += dy;
+    
+    if(game_status == status.playing){
+        ballx += dx;
+        bally += dy;
+    }
 }
-setInterval(Draw, 15);
+setInterval(Draw, 10);
